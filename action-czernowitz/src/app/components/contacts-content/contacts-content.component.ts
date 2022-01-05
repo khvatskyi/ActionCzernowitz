24fc92340e5bc8e59cd2ef6a791c0d97d7b6bea2
Skibinskyi Denys <ruslanskibinskii@gmail.com>
2022-01-05 04:19:01 +0200

    Added routing between content components

---
+++ action-czernowitz/src/app/components/contacts-content/contacts-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-content',
+  templateUrl: './contacts-content.component.html',
+  styleUrls: ['./contacts-content.component.scss']
+})
+export class ContactsContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ action-czernowitz/src/app/components/privacy-content/privacy-content.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrivacyContentComponent } from './privacy-content.component';
+
+describe('PrivacyContentComponent', () => {
+  let component: PrivacyContentComponent;
+  let fixture: ComponentFixture<PrivacyContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrivacyContentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrivacyContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
